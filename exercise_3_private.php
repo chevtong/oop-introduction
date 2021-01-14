9178fd8bff1b1f5e534f4f2bfa7bcaf7a4b326a6
chevtong <chevtongwingyan@gmail.com>
2021-01-14 20:18:20 +0100

    Add ex3

--- exercise_3_private.php
+++ exercise_3_private.php
@@ -11,105 +11,43 @@ Print this method on the screen on a new line.
 USE TYPEHINTING EVERYWHERE!
 */
 declare(strict_types=1);
-class Beverage
-{
-	protected $color;
-	protected $price;
-	protected $temperature;
-	protected $name;
+//require 'exercise_1_classes.php';
 
-	public function __construct(string $color, float $price, string $temperature, string $name)
-	{
-		$this->color = $color;
-		$this->price = $price;
-		$this->temperature = $temperature;
-		$this->name = $name;
-	}
-
-	public function getInfo(): string
-	{
-		return 'The <b>' . $this->name . '</b> is <b>' . $this->temperature . '</b> and <b>' . $this->color . '</b></br>';
-	}
-
-	public function getColor(): string
-	{
-		return $this->color;
-	}
-
-	public function setColor($color)
-	{
-		$this->color = $color;
-
-		return $this;
-	}
-
-	public function getTemperature(): string
-	{
-		return $this->temperature;
-	}
-
-	public function setTemperature($temperature)
-	{
-		$this->temperature = $temperature;
-
-		return $this;
-	}
-}
 
 class Beer extends Beverage
 {
-	private $alcoholpercentage;
+    private string $name;
+    private float $alcoholPercentage;
 
-	public function __construct(string $color, float $price, string $temperature, string $name, float $alcoholpercentage)
-	{
-		$this->color       = $color;
-		$this->price       = $price;
-		$this->temperature = $temperature;
-		$this->name        = $name;
-		$this->alcoholpercentage = $alcoholpercentage;
-	}
+    public function __construct(string $color, float $price,string $temperature, string $name, float $alcoholPercentage)
+    {
+        parent::__construct($color, $price, $temperature="cold");
+        $this->name = $name;
+        $this->alcoholPercentage = $alcoholPercentage;   
+    }
 
-	public function getAlcoholPercentage(): string
-	{
-		return 'The alcohol percentage of <b>' . $this->name . '</b> is <b>' . $this->alcoholpercentage . '%</b>.</br>';
-	}
+     public function getAlcoholPercentage()
+    {
+        return $this->alcoholPercentage;
+    }
 
-	public function beerInfo(): string
-	{
-		return 'Hi, I\'m <b>' . $this->name . '</b> and have an alcohol percentage of <b>' . $this->alcoholpercentage . '%</b>. I\'m <b>' . $this->color . '</b> in color.</br>';
-	}
-	public function getName()
-	{
-		return $this->name;
-	}
+    public function getName()
+    {
+         return $this->name;
+    }
+   
+    public function changeColor($color)
+    {
+        return $this->color = $color;
+    }
 
-	public function setName($name)
-	{
-		$this->name = $name;
+    public function beerInfo()
+    {
+        return "Hi i'm $this->name and have an alochol percentage of $this->alcoholPercentage and I have a $this->color color.";
+    }
 
-		return $this;
-	}
 }
-$duvel = new Beer('blond', 3.50, 'cold', 'duvel', 8.5);
-$guldenDraak = new beer('redish', 4, 'cold', 'Gulden Draak', 10.5);
-// Duvel
-echo '<h3>Duvel</h3>';
-echo ($duvel->getInfo());
-$duvel->setColor('light');
-echo ($duvel->getInfo());
-echo ($duvel->beerInfo());
-echo ($duvel->getAlcoholPercentage());
-echo '</br>';
-// Gulden Draak Test
-echo '<h3>Gulden Draak</h3>';
-echo ($guldenDraak->getInfo());
-$guldenDraak->setColor('amber');
-echo ($guldenDraak->getInfo());
-$guldenDraak->setName('New Gulden Dragon');
-$guldenDraak->setColor('green');
-$guldenDraak->setTemperature('hot');
-echo ($guldenDraak->getInfo());
-echo ($guldenDraak->beerInfo());
-echo ($guldenDraak->getAlcoholPercentage());
 
-?>
+
+
+
--- index.php
+++ index.php
@@ -1,11 +1,7 @@
-
-<?php 
-require 'exercise_1_classes.php';
-require 'exercise_2_extending.php';
-?>
-
 <h3>Exercise 1</h3>
 <?php 
+    require 'exercise_1_classes.php';
+
     $cola = new Beverage("black", 2);
     echo $cola->price . "euro <br>";
     echo $cola->getInfo()."<br>";
@@ -14,11 +10,30 @@ require 'exercise_2_extending.php';
 
 <h3>Exercise 2</h3>
 <?php 
-$duvel = new Beer("blonde", 3.5, "warm","duvel",8.5);
-    echo "Name: ".$duvel->name."<br>";
+    // require 'exercise_2_extending.php';
+
+    // $duvel = new Beer("blonde", 3.5, "warm","duvel",8.5);
+    // echo "Name: ".$duvel->name."<br>";
+    // echo "Color: ".$duvel->color."<br>";
+    // echo "Info: ".$duvel->getInfo()."<br>";
+    // echo $duvel->getAlcoholPercentage()."% <br>";
+    // echo number_format($duvel->getAlcoholPercentage(), 2) . "% <br>";
+    //echo $cola->getAlcoholPercentage()."% <br>";
+?>
+<h3>Exercise 3</h3>
+<?php 
+    require 'exercise_3_private.php';
+
+    $duvel = new Beer("blonde", 3.5, "warm","duvel",8.5);
+
+    echo "Name: ". $duvel->getName()."<br>";
     echo "Color: ".$duvel->color."<br>";
     echo "Info: ".$duvel->getInfo()."<br>";
     echo $duvel->getAlcoholPercentage()."% <br>";
     echo number_format($duvel->getAlcoholPercentage(), 2) . "% <br>";
-    echo $cola->getAlcoholPercentage()."% <br>";
+
+    $duvel->changeColor("light");
+    echo "New Color: " . $duvel->color . "<br>";
+
+    echo $duvel->beerInfo();
 ?>
